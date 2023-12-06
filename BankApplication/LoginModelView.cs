42ea78c0b7edb193e62bf5e6e2440a77293b9813
Ermias Gulti <Egulti4128@conestogac.on.ca>
2023-12-06 02:49:49 -0500

    Added login page form and a code behind it

---
+++ BankApplication/LoginModelView.cs
@@ -0,0 +1,17 @@
+﻿namespace BankApplication
+{
+    public class LoginViewModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public LoginViewModel()
+    {
+        // Initialize non-nullable properties here
+        Username = string.Empty;
+        Password = string.Empty;
+    }
+}
+
+
+}
--- BankApplication/Pages/LoginPage.cshtml.cs
+++ BankApplication/Pages/LoginPage.cshtml.cs
@@ -5,8 +5,33 @@ namespace BankApplication.Pages
 {
     public class LoginPageModel : PageModel
     {
+        public LoginViewModel Login { get; set; }
+
+        public LoginPageModel()
+        {
+            // Initialize the Login property in the constructor
+            Login = new LoginViewModel();
+        }
+
         public void OnGet()
         {
+            // Initialization logic when the page is loaded
+        }
+
+        public IActionResult OnPost()
+        {
+            // Handling form submission logic
+            if (ModelState.IsValid)
+            {
+                // Authenticate user or perform other actions
+                return RedirectToPage("/Index");
+            }
+
+            // If ModelState is not valid, redisplay the form with validation errors
+            return Page();
         }
     }
+
+
 }
+
