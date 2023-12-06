4609f1aa73e859265a6a8bacfb73519a53979d2f
Ermias Gulti <Egulti4128@conestogac.on.ca>
2023-12-06 04:06:09 -0500

    I added 3 more pages which will help us to implement users saving checking and loan accounts activities

---
+++ BankApplication/LoanAccount.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankApplication
+{
+    public class LoanAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
