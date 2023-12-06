8a891f4b4c1cc903d35ac8d685d8857de5dd104f
Ermias Gulti <Egulti4128@conestogac.on.ca>
2023-12-06 16:36:59 -0500

    Few update over the code and I added new page for users Account registeration.

---
+++ BankApplication/Pages/Shared/RegisterModelView.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankApplication.Pages.Shared
+{
+    public class RegisterModelView
+    {
+        // User Information
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; }
+
+        // Additional User Information ..we can add username, password, confirm password stufff
+
+
+        // Optional Information
+        public string Phone { get; set; }
+
+        // Terms and Conditions
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must agree to the terms and conditions.")]
+        public bool AgreeToTerms { get; set; }
+
+        public RegisterModelView()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Phone = string.Empty;
+            AgreeToTerms = false;
+        }
+    }
+}
