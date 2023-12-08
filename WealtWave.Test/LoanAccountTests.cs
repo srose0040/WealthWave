9f5c50e737079c33fc90ead0cd4d7a6f3d778227
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-08 13:37:39 -0500

    Added unit test to check if interest is applied on loan

--- WealtWave.Test/LoanAccountTests.cs
+++ WealtWave.Test/LoanAccountTests.cs
@@ -12,21 +12,42 @@ namespace WealthWave.Test
         public void LoanAccount_ApplyForLoan_GetApprovedForLoanAmount()
         {
             // Arrange varables, classes mocks
-            int amountToWithdraw = 10000;
+            int amountToApplyFor = 10000;
             var userAccount = new LoanAccount();
             string result;
 
 
             // Act
-            userAccount.ApplyForLoan(amountToWithdraw, out result);
+            userAccount.ApplyForLoan(amountToApplyFor, out result);
             
 
             // Assert 
             result.Should().NotBeNullOrWhiteSpace();
             result.Should().Contain("Your loan request has been granted.", Exactly.Once());
             
+        }
+
+        [Theory]
+        [InlineData(10000, 10610, 10611)]
+        public void LoanAccount_ApplyInterestOnLoan_NoticeInterestAppliedToLoan(double testValue, double expectedOutput, double upperRangeOfOutput)
+        {
+            // Arrange varables, classes mocks
+            var userAccount = new LoanAccount();
+            string message;
+            
 
 
+            // Act
+            userAccount.ApplyForLoan(testValue, out message);
+            userAccount.InterestCalculation();
+            var result = userAccount.LoanAmount;
+
+
+            // Assert 
+            result.Should().NotBe(null);
+            // Counts for floating point discrepancy
+            result.Should().BeInRange(expectedOutput, upperRangeOfOutput);
+            result.Should().BePositive();
         }
     }
 }
