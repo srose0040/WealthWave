e27f60d31a493fae7ab8d88ae987942d31a312bf
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-09 18:31:37 -0500

    Added MYSQL reference to project and framework for connecting to database

--- BankApplication1/RegistrationForm.aspx.cs
+++ BankApplication1/RegistrationForm.aspx.cs
@@ -9,6 +9,10 @@ namespace BankApplication1
 {
     public partial class RegistrationForm : System.Web.UI.Page
     {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        MySql.Data.MySqlClient.MySqlCommand cmd;
+        String querystr;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -32,6 +36,23 @@ namespace BankApplication1
             }
 
             
+        }
+
+        private void registerUser()
+        {
+            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebAppConnString"].ToString();
+
+            conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+
+            conn.Open();
+
+            querystr = "";
+
+            cmd = new MySql.Data.MySqlClient.MySqlCommand(querystr, conn);
+
+            cmd.ExecuteReader();
+
+            conn.Close();
         }
 
 
