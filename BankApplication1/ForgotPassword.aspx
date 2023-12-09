da70541043e57d6897b09144000ed4f4befa433c
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-09 16:56:05 -0500

    Set up frame of forgot password page

---
+++ BankApplication1/ForgotPassword.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Language="C#" Title="Forgot Password" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ForgotPassword.aspx.cs" Inherits="BankApplication1.ForgotPassword" %>
+
+<asp:Content ID="Content5" ContentPlaceHolderID="MainContent" runat="server">
+
+    <%-- CREATE HTML TO HANDLE THEM FORGETTING PASSWORD OR REMOVE THIS PAGE --%>
+    <!-- Submit Button -->
+<asp:Button Text="Change Password" runat="server" class="btn btn-primary" style="background-color: darkblue; font-family: 'Times New Roman', Times, serif; font-weight: bold; font-size: 16px; color: white;" OnClick="PasswordButton_Click" />
+
+</asp:Content>
---
+++ BankApplication1/ForgotPassword.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BankApplication1
+{
+    public partial class ForgotPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PasswordButton_Click(object sender, EventArgs e)
+        {
+            // GET ALL INFO FROM THE TEXTBOX THEN 
+            // CONNECT TO DATABASE AND UPDATE CREDENTIALS
+            // Redirect to the next page 
+            Response.Redirect("HomePage.aspx");
+        }
+    }
+}
