b620f6df1dc4bb8020c9fc9406af4a931b14bb93
Ermias Gulti <Egulti4128@conestogac.on.ca>
2023-12-11 17:22:51 -0500

    I have added a new class called 'UserDetails' that will help us retrieve user information from our database. We will use this class to store the information.

---
+++ BankApplication1/UserDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankApplication1
+{
+    public class UserDetails
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public string MaritalStatus { get; set; }
+        public string CountryStatus { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public double CurrentBalance { get; set; }
+        public string Email { get; set; }
+        public int SinNumber { get; set; }
+    }
+}
