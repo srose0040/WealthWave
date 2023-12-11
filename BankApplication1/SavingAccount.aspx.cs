d1c0c136d2c669d368a276d14eced0567700523b
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-10 19:12:35 -0500

    Implementing deposit in savings

--- BankApplication1/SavingAccount.aspx.cs
+++ BankApplication1/SavingAccount.aspx.cs
@@ -53,13 +53,11 @@ namespace BankApplication1
                 balance = reader.GetDouble(reader.GetOrdinal("CurrentBalance"));
                 Session["CustomerBalance"] = balance;
                 customerBalance = balance;
-
-                
+       
             }
             reader.Close();
             conn.Close();
-            
-            
+                     
 
             return balance;
         }
@@ -69,24 +67,41 @@ namespace BankApplication1
         {
             double depositAmount = Convert.ToDouble(amountTextBox.Text);
 
-            
-
-            //// Create an instance of the SavingsAccount class
-            //SavingsAccount savingsAccount = new SavingsAccount();
-
-            //// Retrieve user balance from the database
-            //double currentBalance = GetBalanceFromDatabase(userId);
-
             //Initialize the SavingsAccount instance with the retrieved balance
             savingsAccount.CurrentBalance = customerBalance;
 
             //// Perform deposit transaction
             string message;
             savingsAccount.DepositTransaction(depositAmount, out message);
+            // MESSAGE MUST BE PRESENTED TO USER
 
             //// Update the database with the new balance
-            //UpdateBalanceInDatabase(userId, savingsAccount.CurrentBalance);
+            UpdateBalanceInDatabase(userID, savingsAccount.CurrentBalance);
         }
 
+        // Method to retrieve user balance from the database
+        private double UpdateBalanceInDatabase(int userId, double currentBalance)
+        {
+
+            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebAppConnString"].ToString();
+            conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+            conn.Open();
+            querystr = "";
+            querystr = "SELECT CurrentBalance FROM bankapplication.customer WHERE CustomerId='" + userID.ToString() + "'";
+            cmd = new MySql.Data.MySqlClient.MySqlCommand(querystr, conn);
+            reader = cmd.ExecuteReader();
+            while (reader.HasRows & reader.Read())
+            {
+                balance = reader.GetDouble(reader.GetOrdinal("CurrentBalance"));
+                Session["CustomerBalance"] = balance;
+                customerBalance = balance;
+
+            }
+            reader.Close();
+            conn.Close();
+
+
+            return balance;
+        }
     }
 }
