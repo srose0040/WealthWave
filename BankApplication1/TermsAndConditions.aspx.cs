7db72a31f54a56cbe51e2daf1181ebe9ea61a61d
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2024-11-08 12:08:13 -0500

    Fixed the bug on the registration page. For some reason, the Terms and Conditions page was deleted, so I added it back. Now, users can read the terms and conditions, and when they agree, they will be redirected to the registration page. Additionally, the app’s start page has been changed to the login page

---
+++ BankApplication1/TermsAndConditions.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.UI;
+
+namespace BankApplication1
+{
+    public partial class TermsAndConditions : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        protected void AgreeButton_Click(object sender, EventArgs e)
+        {
+            
+            // Set a session variable to track that the user agreed to the terms
+            Session["TermsAccepted"] = true;
+
+            //redirecting the back to register page
+            Response.Redirect("RegistrationForm.aspx"); 
+        }
+    }
+}
